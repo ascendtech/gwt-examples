27b50bb7a0df37ebe67e7e928e632713ee5d1ea3
Matthew Davis <matt.davis@ascend-tech.us>
2020-11-18 13:52:03 -0500

    upgrade gradle and plugin version

--- todoMaterial/build.gradle.kts
+++ todoMaterial/build.gradle.kts
@@ -6,7 +6,7 @@ gwt {
     modules.add("us.ascendtech.ToDo")
     libs.add("vue")
     libs.add("autorest")
-    libs.add("elemento-core-1.0")
+    libs.add("elemento-core")
     libs.add("ast-highcharts")
     libs.add("ast-aggrid")
     libs.add("ast-momentjs")
