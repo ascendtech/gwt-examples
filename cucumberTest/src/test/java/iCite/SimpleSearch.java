7a7143b8b0f530dd0e296323e9fb6b5e4ef69401
Lawrence Luna <l.luna@digital-science.com>
2021-04-21 15:25:25 -0400

    Added Test for using AND operator.

--- cucumberTest/src/test/java/iCite/SimpleSearch.java
+++ cucumberTest/src/test/java/iCite/SimpleSearch.java
@@ -12,11 +12,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-
+//SimpleSearch
 public class SimpleSearch {
-
     private WebDriver driver;
-
 
     @Given("I am on the iCite page.")
     public void iAmOnTheiCitePage() {
@@ -25,15 +23,12 @@ public class SimpleSearch {
         //lets the application compile
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
-
     @When("I click my mouse in the Search query field.")
     public void iClickInTheSearchQueryField()
-
     {
        // driver.findElement(By.id("searchBox")).click();
         WebElement element = driver.findElement(By.id("searchBox"));
         WebDriverWait wait = new WebDriverWait(driver, 20); //here, wait time is 20 seconds
-
         wait.until(ExpectedConditions.visibilityOf(element)); //this will wait for element to be visible for 20 seconds
         element.click(); //now it clicks on element
     };
@@ -41,12 +36,10 @@ public class SimpleSearch {
     public void ienterpcsk9() {
         driver.findElement(By.id("searchBox")).sendKeys("pcsk9");
     }
-
     @When("I click the magnifying glass.")
     public void magnifyingGlass() {
         driver.findElement(By.id("searchButton")).click();
     }
-
     @Then("the results for pcsk9 will be displayed.")
     public void theResultsWillBeDisplayed()
 
@@ -54,4 +47,41 @@ public class SimpleSearch {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+//Simple Search using AND function
+
+    @When("I search for COVID and SYMPTOMS.")
+    public void isearchforcovidandsymptoms()
+
+    {
+        driver.findElement(By.id("searchBox")).sendKeys("COVID and SYMPTOMS");
+    }
+
+    @Then("all results for COVID as well as SYMPTOMS will be displayed.")
+    public void alltheresultsforcovidaswellassymptomswillbedisplayed()
+
+    {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+//Opening the help menu
+
+    @When("I click the menu button.")
+    public void iclickthemenubutton()
+    {
+        driver.findElement(By.id("app-options")).click();
+    }
+
+    @When("I click the User Guide link.")
+    public void iclicktheuserguidelink()
+    {
+        driver.findElement(By.id("User Guide")).click();
+    }
+
+    @Then("the User Guide will be displayed.")
+    public void theuserguidewillbedisplayed()
+
+    {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 }
+
