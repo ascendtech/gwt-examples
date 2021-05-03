973c63e67434f5a98f6cdf508eeba40660e56ed2
Lawrence Luna <l.luna@digital-science.com>
2021-05-03 17:18:01 -0400

    Added Test for using AND operator.

--- cucumberTest/src/test/java/iCite/SimpleSearch.java
+++ cucumberTest/src/test/java/iCite/SimpleSearch.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
+
 //SimpleSearch
 public class SimpleSearch {
     private WebDriver driver;
@@ -23,65 +24,109 @@ public class SimpleSearch {
         //lets the application compile
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
+
     @When("I click my mouse in the Search query field.")
-    public void iClickInTheSearchQueryField()
-    {
-       // driver.findElement(By.id("searchBox")).click();
+    public void iClickInTheSearchQueryField() {
+        // driver.findElement(By.id("searchBox")).click();
         WebElement element = driver.findElement(By.id("searchBox"));
         WebDriverWait wait = new WebDriverWait(driver, 20); //here, wait time is 20 seconds
         wait.until(ExpectedConditions.visibilityOf(element)); //this will wait for element to be visible for 20 seconds
         element.click(); //now it clicks on element
-    };
+    }
+
     @When("I enter pcsk9.")
     public void ienterpcsk9() {
         driver.findElement(By.id("searchBox")).sendKeys("pcsk9");
     }
+
     @When("I click the magnifying glass.")
     public void magnifyingGlass() {
         driver.findElement(By.id("searchButton")).click();
     }
-    @Then("the results for pcsk9 will be displayed.")
-    public void theResultsWillBeDisplayed()
 
-    {
+    @Then("the results for pcsk9 will be displayed.")
+    public void theResultsWillBeDisplayed() {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
 //Simple Search using AND function
 
     @When("I search for COVID and SYMPTOMS.")
-    public void isearchforcovidandsymptoms()
-
-    {
+    public void isearchforcovidandsymptoms() {
         driver.findElement(By.id("searchBox")).sendKeys("COVID and SYMPTOMS");
     }
 
     @Then("all results for COVID as well as SYMPTOMS will be displayed.")
-    public void alltheresultsforcovidaswellassymptomswillbedisplayed()
-
-    {
+    public void alltheresultsforcovidaswellassymptomswillbedisplayed() {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-//Opening the help menu
+//Simple Search using OR function
 
-    @When("I click the menu button.")
-    public void iclickthemenubutton()
-    {
-        driver.findElement(By.id("app-options")).click();
+    @When("I search for COVID or SYMPTOMS.")
+    public void isearchforcovidorsymptoms() {
+        driver.findElement(By.id("searchBox")).sendKeys("COVID or SYMPTOMS");
     }
 
-    @When("I click the User Guide link.")
-    public void iclicktheuserguidelink()
+    @Then("all results for COVID or SYMPTOMS will be displayed.")
+    public void alltheresultsforcovidorsymptomswillbedisplayed() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    //Simple Search using a date range
+
+    @When("I click advancedFilter.")
+    public void iclickadvancedFilter()
     {
-        driver.findElement(By.id("User Guide")).click();
+        //driver.findElement(By.id("advancedFilter"));
+        WebElement element = driver.findElement(By.id("advancedFilter"));
+        WebDriverWait wait = new WebDriverWait(driver, 20); //here, wait time is 20 seconds
+        wait.until(ExpectedConditions.visibilityOf(element)); //this will wait for element to be visible for 20 seconds
+        element.click(); //now it clicks on element
     }
 
-    @Then("the User Guide will be displayed.")
-    public void theuserguidewillbedisplayed()
+    @When("I click the From box.")
+    public void iclickthefrombox()
 
+    {
+        //"FROM BOX"
+        WebElement element = driver.findElement(By.xpath("//input[@class='form-control']"));
+        WebDriverWait wait = new WebDriverWait(driver, 5); //here, wait time is 5 seconds
+        wait.until(ExpectedConditions.visibilityOf(element)); //this will wait for element to be visible for 20 seconds
+        element.click(); //now it clicks on element
+    }
+    @When("I enter 2021-03-03.")
+    public void ienter20210303()
+    {
+        //"Enters start date. This is the line for the FROM Date Picker")
+        driver.findElement(By.xpath("//input[@class='form-control']")).sendKeys("2021-03-03");
+    }
+
+    @When("I click the To box.")
+    public void iclickthetobox()
+    {
+        //"TO BOX"
+        WebElement element = driver.findElement(By.xpath("(//input[@class='form-control'])[2]"));
+        WebDriverWait wait = new WebDriverWait(driver, 5); //here, wait time is 5 seconds
+        wait.until(ExpectedConditions.visibilityOf(element)); //this will wait for element to be visible for 20 seconds
+        element.click(); //now it clicks on element
+    }
+    @When("I enter 2021-04-26.")
+    public void ienter20210426()
+    {
+        //"Enters start date. This is the line for the To Date Picker")
+        driver.findElement(By.xpath("(//input[@class='form-control'])[2]")).sendKeys("2021-04-26");
+
+    }
+
+    @When("I click the Apply filters button.")
+    public void iclicktheapplyfiltersbutton()
+    {
+        //Clicks apply filter button
+        driver.findElement(By.xpath("//button[contains(@class,'btn btn-sm')]")).click();
+    }
+    @Then("all publications will be displayed.")
+    public void allpublicationswithinthedaterangewillbedisplayedandiamdonewithmytest()
     {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 }
-
