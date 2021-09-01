f78f52161a6465e7ca27d549df25f0595f3ca149
Matthew Davis <matt.davis@ascend-tech.us>
2021-09-01 10:56:02 -0400

    blind stab in the dark

--- todoRest/src/main/java/us/ascendtech/rest/controllers/UploadController.java
+++ todoRest/src/main/java/us/ascendtech/rest/controllers/UploadController.java
@@ -6,6 +6,9 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.StreamingFileUpload;
+import io.micronaut.scheduling.TaskExecutors;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 import us.ascendtech.rest.dto.DropzoneResponse;
@@ -13,14 +16,20 @@ import us.ascendtech.rest.dto.ToDo;
 import us.ascendtech.rest.services.ToDoService;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
 
 @Controller("/service")
 public class UploadController {
 
 	private ToDoService todoService;
+
+	@Inject
+	@Named(TaskExecutors.IO)
+	ExecutorService ioExecutor;
 
 	public UploadController(ToDoService todoService) {
 		this.todoService = todoService;
@@ -30,7 +39,7 @@ public class UploadController {
 	public Publisher<HttpResponse<DropzoneResponse>> upload(StreamingFileUpload file) throws IOException {
 
 		File tempFile = File.createTempFile(file.getFilename(), "temp");
-		Publisher<Boolean> uploadPublisher = file.transferTo(tempFile);
+		Publisher<Boolean> uploadPublisher = TransferToStream.transferToStream(ioExecutor, file, new FileOutputStream(tempFile));
 
 		return Mono.from(uploadPublisher).map(success -> {
 			if (success) {
